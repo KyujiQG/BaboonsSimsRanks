462553ba6fc11c7cea9d7933b8509eb5e2836f63
Number97 <kyuji.qg@gmail.com>
2023-01-19 11:22:25 +0100

    big changes. noice!

---
+++ script.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let htmlString = "";
+let counter = 1;
+
+fs.readFile('game\\FORMAT.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.createReadStream('file.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        htmlString += `<tr class='${row.class}'>\n`
+        htmlString += `   <td class='character-name'>${row.character_name}</td>\n`
+        htmlString += `   <td class='rank'>#${counter}</td>\n`
+        htmlString += `   <td class='DPS'>\n      <a href='${row.link}' target='_blank'>${row.dps} DPS</a>\n   </td>\n`
+        htmlString += "</tr>"
+        counter += 1;
+    })
+    .on('end', () => {
+        let updatedData = data.replace("<tbody>", "<tbody>\n" + htmlString);
+        fs.writeFileSync('game\\index.html', updatedData);
+        console.log('HTML file generated.');
+    });
+});
